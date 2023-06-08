a58d5d6287d8a0002a251f4cdf537dac0906cc90
jclmnop <jcheshire@protonmail.com>
2023-06-08 16:32:26 +0100

    impl sensor-reader actor

--- wasmcloud/actors/sensor-reader/src/lib.rs
+++ wasmcloud/actors/sensor-reader/src/lib.rs
@@ -1,15 +1,48 @@
 use wasmbus_rpc::actor::prelude::*;
-use wasmcloud_interface_httpserver::{HttpRequest, HttpResponse, HttpServer, HttpServerReceiver};
+use wasmcloud_interface_polling::{PollResult, PollSubscriber, PollSubscriberReceiver};
+use wasmcloud_interface_logging::{error, debug, info};
+use actor_interfaces::{PangeaApiSender, LogEvent, PangeaApi};
+
+const PANGEA_API_ACTOR: &str = "pangea_api";
 
 #[derive(Debug, Default, Actor, HealthResponder)]
-#[services(Actor, HttpServer)]
+#[services(Actor, PollSubscriber)]
 struct SensorReaderActor {}
 
-/// Implementation of the HttpServer capability contract
 #[async_trait]
-impl HttpServer for SensorReaderActor {
-    async fn handle_request(&self, _ctx: &Context, _req: &HttpRequest) -> RpcResult<HttpResponse> {
-        Ok(HttpResponse::ok("Hello, World!"))
+impl PollSubscriber for SensorReaderActor {
+    // TODO: deserialize reading/result
+    async fn poll_rx(&self, ctx: &Context, poll_result: &PollResult) -> RpcResult<()> {
+        info!("sensor_reader: processing received poll result");
+        if let Some(poll_error) = poll_result.error.clone() {
+            error!(
+                "Error polling sensors, \n\tERROR_TYPE: {}\n\tDESCRIPTION: {}\n",
+                poll_error.error_type,
+                poll_error.description.unwrap_or("n/a")
+            );
+        } else {
+            if let Some(poll_readings) = data {
+                match serde_json::from_slice::<Vec<LogEvent>>(&poll_readings) {
+                    Ok(poll_readings) => {
+                        info!("Sending readings to event log");
+                        let pangea_api = PangeaApiSender::to_actor(PANGEA_API_ACTOR);
+                        let write_result = pangea_api.write_audit_log(ctx, &poll_readings).await?;
+                        if write_result.success {
+                            info!("Successfully wrote readings to audit log");
+                        } else {
+                            error!("Error writing to audit log: {}", write_result.reason.unwrap_or(""))
+                        }
+                    }
+                    Err(e) => {
+                        error!("Failed to deserialize sensor readings: {e}");
+                    }
+                }
+            } else {
+                error!("No sensor readings contained in poll result");
+            }
+        }
+        Ok(())
     }
 }
+
 
