6d750f6367ba1ed26ec25c48fe236b7a3fbc4863
jclmnop <jcheshire@protonmail.com>
2023-06-08 13:06:04 +0100

    add actor skeletons

---
+++ wasmcloud/actors/pangea-api/src/lib.rs
@@ -0,0 +1,15 @@
+use wasmbus_rpc::actor::prelude::*;
+use wasmcloud_interface_httpserver::{HttpRequest, HttpResponse, HttpServer, HttpServerReceiver};
+
+#[derive(Debug, Default, Actor, HealthResponder)]
+#[services(Actor, HttpServer)]
+struct PangeaApiActor {}
+
+/// Implementation of the HttpServer capability contract
+#[async_trait]
+impl HttpServer for PangeaApiActor {
+    async fn handle_request(&self, _ctx: &Context, _req: &HttpRequest) -> RpcResult<HttpResponse> {
+        Ok(HttpResponse::ok("Hello, World!"))
+    }
+}
+
